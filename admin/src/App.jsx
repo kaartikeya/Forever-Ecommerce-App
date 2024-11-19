d9ffbee8d8e8362e44a236120cd5a833af5776e8
kaartikeya <kaartikeya.1@gmail.com>
2024-11-19 18:42:52 +0530

    change to vercel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export const backendUrl = "https://forever-ecommerce-website-e4dc.onrender.com";
+export const backendUrl = "https://ecommerce-backend-six-psi.vercel.app";
 export const currency = "$";
 
 const App = () => {
